9d8421fe0e40fdf3f4f32db8bd12b025e11694cc
Hami-Hafezi <dev.HamiHafezi@gmail.com>
2025-01-17 03:32:51 +0330

    Print Hello World

---
+++ Hami_Java/Java_twoTheSquare.java
@@ -0,0 +1,12 @@
+package Hami_Java;
+
+import java.util.Scanner;
+
+public class Java_twoTheSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        number = number * number;
+        System.out.println(number);
+    }
+}
--- Hami_Java/java_one.java
+++ Hami_Java/java_one.java
@@ -3,5 +3,6 @@ package Hami_Java;
 public class java_one {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
     }
 }
